f4fb5fa7ae04c70499cdb65fac53b37c863cd5fa
AhmedMohsen2001 <1901061@eng.asu.edu.eg>
2024-07-13 10:41:08 +0300

    What Happened

--- Labs_Week_3.py
+++ Labs_Week_3.py
@@ -1,3 +1,8 @@
 import numpy as np
-from sklearn.linear_model import SGDRegressor, LinearRegression
+from sklearn.linear_model import SGDRegressor, LinearRegression, LogisticRegression
 from sklearn.preprocessing import StandardScaler
+
+def perceptron_init():
+    X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
+    y_train = np.array([0, 0, 0, 1, 1, 1])                                       #(m,)
+    
